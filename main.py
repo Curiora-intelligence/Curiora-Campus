c4f090cb4dcde084d0edeff1be7f9d23f198e492
saiganesh <saiganeshsattenapalli828@gmail.com>
2026-08-19 12:53:45 +0530

    initiated the session middleware

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ installed.
 
 from __future__ import annotations
 
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 from app.routers.authentication import auth_router
 from app.routers.campus import campus_router
 from app.routers.curio import curio_router
 
 app = FastAPI(title="saiganesh",version="0.141.0",docs_url=None,openapi_external_docs=None,redoc_url=None,openapi_url=None)
-
+app.add_middleware(SessionMiddleware,secret_key=os.get("secret_key"))
 templates = Jinja2Templates(directory="templates")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(auth_router)
 app.include_router(campus_router)
